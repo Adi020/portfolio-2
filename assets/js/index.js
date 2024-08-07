4e6514c22160dd834f43cad02bd4a374dbc60cc5
Adi <121740959+Adi020@users.noreply.github.com>
2024-08-06 19:42:36 -0600

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ import featuredProjectsES from "../components/featuredProjectsES.js";
 import { handleCheckClick } from "../components/handleCheckClick .js";
 import { handleLoadEvent } from "../components/handleLoadEvent.js";
 import { handleShowProjectsClick } from "../components/handleShowProjectsClick.js";
+import { observeProject } from "../components/observeProject.js";
 import { observeSections } from "../components/observeSections.js";
 import printFeaturedProjects from "../components/printFeaturedProjects.js";
 import printProjects from "../components/printProjects.js";
@@ -20,9 +21,9 @@ let headerBtn = document.querySelector(".header__btn");
 let sections = document.querySelectorAll("section");
 let links = document.querySelectorAll(".list__link");
 const pointer = document.querySelector(".cursor");
-const toast = document.querySelector(".toast")
-const closeIcon = document.querySelector(".close")
-const progress = document.querySelector(".progress")
+const toast = document.querySelector(".toast");
+const closeIcon = document.querySelector(".close");
+const progress = document.querySelector(".progress");
 
 if (check.checked) {
   printProjects(projectsEN, check, portfolioContainer);
@@ -38,11 +39,11 @@ showProjects.addEventListener("click", (e) =>
   handleShowProjectsClick(e, portfolioContainer, showDegradiant)
 );
 closeIcon.addEventListener("click", () => {
-  toast.classList.remove("active")
+  toast.classList.remove("active");
   setTimeout(() => {
-    progress.classList.remove("active")
-  }, 300)
-})
+    progress.classList.remove("active");
+  }, 300);
+});
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -52,12 +53,10 @@ form.addEventListener("submit", function (e) {
     email: formData.get("email"),
     message: formData.get("message"),
   };
-  emailjs
-    .send("service_9hmvrez", "template_aiuhovu", data)
-    .then((res) => {
-      showToast()
-      this.reset()
-    });
+  emailjs.send("service_9hmvrez", "template_aiuhovu", data).then((res) => {
+    showToast();
+    this.reset();
+  });
 });
 
 const swiper = new Swiper(".portfolio .swiper", {
@@ -84,6 +83,10 @@ const swiper = new Swiper(".portfolio .swiper", {
 window.addEventListener("load", () => handleLoadEvent(pointer));
 
 observeSections(sections, links);
+
+let projectsInfo = document.querySelectorAll(".portfolio__item");
+
+observeProject(projectsInfo);
 
 function addEvents() {
   window.addEventListener("scroll", handleScroll);
@@ -118,14 +121,13 @@ function toggleMenu() {
 
 function showToast() {
   toast.classList.add("active");
-  progress.classList.add("active")
+  progress.classList.add("active");
 
   setTimeout(() => {
-    toast.classList.remove("active")
-  }, 5000)
+    toast.classList.remove("active");
+  }, 5000);
 
   setTimeout(() => {
-    progress.classList.remove("active")
-  }, 5300)
-
+    progress.classList.remove("active");
+  }, 5300);
 }
