de41cecb88df9d8da507f34829cd4ae99d823d20
Adi <121740959+Adi020@users.noreply.github.com>
2024-08-06 19:44:34 -0600

    Update projectsES.js

--- assets/components/projectsES.js
+++ assets/components/projectsES.js
@@ -1,55 +1,103 @@
 const projectsES = [
-	{
-		projectName: "aplicación de frase aleatoria",
-		projectDesktopImg: "../assets/img/FraseRandom.jpg",
-		projectMobileImg: "../assets/img/FraseRandomMovil.jpg",
-		projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
+  {
+    projectName: "aplicación de frase aleatoria",
+    projectDesktopImg: "../assets/img/FraseRandom.jpg",
+    projectMobileImg: "../assets/img/FraseRandomMovil.jpg",
+    projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
 		con persistencia en local storage para generar phases aleatoria y mostrar imagenes random junto con su autor, ademas la interfazes 
 		responsiva para adaptarse a cualquier dispositivo`,
-	},
-	{
-		projectName: "crud de usuarios",
-		projectDesktopImg: "../assets/img/ConsultaDeUsuarios.webp",
-		projectMobileImg: "../assets/img/ConsultaDeUsuariosMovil.jpg",
-		projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://entregable-gen26.netlify.app/",
+  },
+  {
+    projectName: "crud de usuarios",
+    projectDesktopImg: "../assets/img/ConsultaDeUsuarios.webp",
+    projectMobileImg: "../assets/img/ConsultaDeUsuariosMovil.jpg",
+    projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
 		Implementé el consumo de API, listando usuarios, utilizando metodos para agregar, eliminar y actualizar los usuarios, agregue modo de
 		noche y modo de día, ademas de que la interfaz es responsiva adaptandose a cualquier dispositivo`,
-	},
-	{
-		projectName: "E-commerce con logueo de usuarios ",
-		projectDesktopImg: "../assets/img/E-commerce.png",
-		projectMobileImg: "../assets/img/E-commerce-movil.jpg",
-		projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://entregable4-gen-26-react.netlify.app/",
+  },
+  {
+    projectName: "E-commerce con logueo de usuarios ",
+    projectDesktopImg: "../assets/img/E-commerce.png",
+    projectMobileImg: "../assets/img/E-commerce-movil.jpg",
+    projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, incluyendo ReactJS y TailwindCSS.
 		Utilicé librerías como Redux para manejar los estados globales , react-router-dom para el enrutado y manejo de rutas protegidas, 
 		Axios para manejar las peticiones de manera rápida y react-hook-form para manejar la información recibida por formularios de manera
 		más sencilla.`,
-	},
-	{
-		projectName: "carrito de compras",
-		projectDesktopImg: "../assets/img/Carrito.webp",
-		projectMobileImg: "../assets/img/CarritoMovil.jpg",
-		projectText: `En el desarrollo de esta pagina web, empleé una combinación de 3 tecnologías como HTML CSS y Javascript. 
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://e-commerce-a-pp.netlify.app/",
+  },
+  {
+    projectName: "carrito de compras",
+    projectDesktopImg: "../assets/img/Carrito.webp",
+    projectMobileImg: "../assets/img/CarritoMovil.jpg",
+    projectText: `En el desarrollo de esta pagina web, empleé una combinación de 3 tecnologías como HTML CSS y Javascript. 
     implementé el consumo de API, para mostrar los articulos, añadí un filtrado por categoria, agregado de articulos al carrito,
     suma total de los productos agregador y compra de articulos, ademas de que diseñe la interfaz para ser responsiva`,
-	},
-	{
-		projectName: "aplicación de pokemones",
-		projectDesktopImg: "../assets/img/Pokedex.webp",
-		projectMobileImg: "../assets/img/PokedexMovil.jpg",
-		projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas como reactJS, 
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://shopping-cart-acc.netlify.app/",
+  },
+  {
+    projectName: "aplicación de pokemones",
+    projectDesktopImg: "../assets/img/Pokedex.webp",
+    projectMobileImg: "../assets/img/PokedexMovil.jpg",
+    projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas como reactJS, 
     TailwindCSS y librerias como redux y react router doom. Implementé el consumo de API, complemente la pagina con 
     alguna funcionalidades extra tal como el filtrado por categoria y busqueda de un pokemon especifico tambien diseñé
     la interfaz para ser responsiva`,
-	},
-	{
-		projectName: "aplicación del tiempo",
-		projectDesktopImg: "../assets/img/Clima.webp",
-		projectMobileImg: "../assets/img/ClimaMovil.webp",
-		projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, i
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://pokedex-proyectt.netlify.app/",
+  },
+  {
+    projectName: "aplicación del tiempo",
+    projectDesktopImg: "../assets/img/Clima.webp",
+    projectMobileImg: "../assets/img/ClimaMovil.webp",
+    projectText: `En el desarrollo de esta aplicación, empleé una combinación de tecnologías avanzadas, i
     ncluyendo ReactJS y TailwindCSS. Implementé el consumo de API, solicitando permisos y proporcionando 
     la ubicación del usuario para realizar consultas precisas. Además, diseñé la interfaz para ser responsiva, 
     asegurando una experiencia de usuario óptima en cualquier tipo de dispositivo`,
-	},
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://weather-time-site.netlify.app/",
+  },
 ];
 
 export default projectsES;
