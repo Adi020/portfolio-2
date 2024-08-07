3ff58911fa61cfd2004b890f034a8985751b4692
Adi <121740959+Adi020@users.noreply.github.com>
2024-08-06 19:46:40 -0600

    Update projectsEN.js

--- assets/components/projectsEN.js
+++ assets/components/projectsEN.js
@@ -1,67 +1,115 @@
 const projectsEN = [
-	{
-		projectName: "random phrase app",
-		projectDesktopImg: "../assets/img/FraseRandom.jpg",
-		projectMobileImg: "../assets/img/FraseRandomMovil.jpg",
-		projectText: `In developing this application, I employed a combination of advanced technologies, 
+  {
+    projectName: "random phrase app",
+    projectDesktopImg: "../assets/img/FraseRandom.jpg",
+    projectMobileImg: "../assets/img/FraseRandomMovil.jpg",
+    projectText: `In developing this application, I employed a combination of advanced technologies, 
 		including ReactJS and TailwindCSS. with persistence in local storage to generate random phases and 
-		display random images along with their author, plus the responsive interface to adapt to any device`
-	},
-	{
-		projectName: "users crud",
-		projectDesktopImg: "../assets/img/ConsultaDeUsuarios.webp",
-		projectMobileImg: "../assets/img/ConsultaDeUsuariosMovil.jpg",
-		projectText: `In developing this application, I employed a combination of
+		display random images along with their author, plus the responsive interface to adapt to any device`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://entregable-gen26.netlify.app/",
+  },
+  {
+    projectName: "users crud",
+    projectDesktopImg: "../assets/img/ConsultaDeUsuarios.webp",
+    projectMobileImg: "../assets/img/ConsultaDeUsuariosMovil.jpg",
+    projectText: `In developing this application, I employed a combination of
       advanced technologies, including ReactJS and TailwindCSS. I
       implemented API consumption, listing users, using methods to
       add, delete and update users, added night mode and day mode, plus the interface is
-      responsive adapting to any device`
-	},
-	{
-		projectName: "e-commerce with user login",
-		projectDesktopImg: "../assets/img/E-commerce.png",
-		projectMobileImg: "../assets/img/E-commerce-movil.jpg",
-		projectText: `When developing this application, I employed a combination of
+      responsive adapting to any device`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://entregable4-gen-26-react.netlify.app/",
+  },
+  {
+    projectName: "e-commerce with user login",
+    projectDesktopImg: "../assets/img/E-commerce.png",
+    projectMobileImg: "../assets/img/E-commerce-movil.jpg",
+    projectText: `When developing this application, I employed a combination of
       advanced technologies including ReactJS and TailwindCSS. I
       used libraries like Redux to manage global states, react-router-dom to route and manage protected
       routes, Axios to handle requests quickly and react-hook-form
       to handle the information received by forms in a simpler
-      way.`
-	},
-	{
-		projectName: "shopping cart",
-		projectDesktopImg: "../assets/img/Carrito.webp",
-		projectMobileImg: "../assets/img/CarritoMovil.jpg",
-		projectText: `In the development of this website, I used a combination of 3
+      way.`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://e-commerce-a-pp.netlify.app/",
+  },
+  {
+    projectName: "shopping cart",
+    projectDesktopImg: "../assets/img/Carrito.webp",
+    projectMobileImg: "../assets/img/CarritoMovil.jpg",
+    projectText: `In the development of this website, I used a combination of 3
       technologies such as HTML, CSS, and Javascript. I implemented
       API consumption, to display the items, I added a filter by category, added items to the cart, total
       sum of the aggregator products and item purchase, in
-      addition to designing the interface to be responsive`
-	},
-	{
-		projectName: "pokemon app",
-		projectDesktopImg: "../assets/img/Pokedex.webp",
-		projectMobileImg: "../assets/img/PokedexMovil.jpg",
-		projectText: `In the development of this application, I employed a
+      addition to designing the interface to be responsive`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://shopping-cart-acc.netlify.app/",
+  },
+  {
+    projectName: "pokemon app",
+    projectDesktopImg: "../assets/img/Pokedex.webp",
+    projectMobileImg: "../assets/img/PokedexMovil.jpg",
+    projectText: `In the development of this application, I employed a
       combination of advanced technologies such as reactJS,
       TailwindCSS, and libraries such as redux and react router
       doom. I implemented API consumption, I complemented the page
       with some extra functionalities such as filtering by
       category and searching for a specific pokémon, I also
-      designed the interface to be responsive`
-	},
-	{
-		projectName: "weather app",
-		projectDesktopImg: "../assets/img/Clima.webp",
-		projectMobileImg: "../assets/img/ClimaMovil.jpg",
-		projectText: `In the development of this application, I employed a
+      designed the interface to be responsive`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://pokedex-proyectt.netlify.app/",
+  },
+  {
+    projectName: "weather app",
+    projectDesktopImg: "../assets/img/Clima.webp",
+    projectMobileImg: "../assets/img/ClimaMovil.jpg",
+    projectText: `In the development of this application, I employed a
       combination of advanced technologies, including ReactJS and
       TailwindCSS. I implemented API consumption, requesting
       permissions and providing the user's location to make precise
       queries. In addition, I designed the interface to be
       responsive, ensuring an optimal user experience on any type of
-      device`
-	},
+      device`,
+    technologies: [
+      { name: "ReactJS", icon: "../../public/svg/icons/react.svg" },
+      { name: "TailwindCSS", icon: "../../public/svg/icons/tailwind.svg" },
+      { name: "NodeJS", icon: "../../public/svg/icons/nodejs.svg" },
+      { name: "ExpressJS", icon: "../../public/svg/icons/express.svg" },
+      { name: "Sequelize", icon: "../../public/svg/icons/sequelize.svg" },
+    ],
+    link: "https://weather-time-site.netlify.app/",
+  },
 ];
 
 export default projectsEN;
